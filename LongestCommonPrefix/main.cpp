c35543e1f5c25516120dd27d1433d522d1ced395
zhengji <zhengji@youmi.net>
2015-06-24 09:55:53 +0800

    添加最长子串的答题感悟

--- LongestCommonPrefix/main.cpp
+++ LongestCommonPrefix/main.cpp
@@ -3,15 +3,15 @@ class Solution {
     public:
         string longestCommonPrefix(vector<string>& strs) {
             if (strs.empty()) {
-                return strs
+                return strs;
             }
             for (i = 0; i < strs[0].size(); i++) {
                 for (j = 1; j < strs.size(); j++) {
-                    if (i == strs[j].size) {
-                        return strs[j]
+                    if (i == strs[j].size()) {
+                        return strs[j];
                     }
-                    if (i < strs[j].size && strs[0][i] != strs[j][i]) {
-                        return strs.substr(0, i)
+                    if (i < strs[j].size() && strs[0][i] != strs[j][i]) {
+                        return strs.substr(0, i);
                     }
                 }
             }
